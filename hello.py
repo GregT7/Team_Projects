7dc40ecd50afdc67f7503b58c8b0c108a66adecf
JordanCuccia <107869202+JordanCuccia@users.noreply.github.com>
2025-02-25 09:00:30 -0500

    Update hello.py

--- hello.py
+++ hello.py
@@ -5,7 +5,7 @@ import time
 radio = RF24(17, 0)  # CE=GPIO22, CSN=SPI CE0 (GPIO8)
 radio.begin()
 radio.setPALevel(2)  # Set power level
-radio.setDataRate(RF24.RF24_2MBPS)  # Set data rate
+radio.setDataRate(rf24_datarate_e.RF24_2MBPS)  # Set data rate
 radio.openWritingPipe(0xF0F0F0F0E1)  # Unique pipe address
 radio.stopListening()
 time.sleep(0.1)
