4fa3331f96ed9821b7482363132e2063b22bc2eb
GregT7 <gregterrelga@gmail.com>
2025-03-24 10:31:50 -0400

    debug test 4

--- src/classify_driver.py
+++ src/classify_driver.py
@@ -5,7 +5,8 @@ import config
 if __name__ == '__main__':
     start = time.time()
     features = clf.extract_features(config.params)
-    kNN = clf.train_model(features['data'], features['labels'], config.params)
-    stats = clf.test_model(kNN, config.params, move_files=False) # change to True to move classified files
-    stats['time'] = round(time.time() - start, 2)
-    clf.print_accuracy(stats['ds'], stats['nds'], stats['time'], stats['misclassified_images'], disp_images=True)
+    
+    # kNN = clf.train_model(features['data'], features['labels'], config.params)
+    # stats = clf.test_model(kNN, config.params, move_files=False) # change to True to move classified files
+    # stats['time'] = round(time.time() - start, 2)
+    # clf.print_accuracy(stats['ds'], stats['nds'], stats['time'], stats['misclassified_images'], disp_images=True)
--- src/image_classifier.py
+++ src/image_classifier.py
@@ -161,6 +161,8 @@ def extract_features(params):
     labels = []
 
     train_paths = get_file_paths(params['train_path'])
+    write_to_text("path_names.txt", train_paths)
+    # print(train_paths)
 
     # loop over the image paths in the training set
     img_strs = []
